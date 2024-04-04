6a0d674cb7eaafaef7482e8dd44eb8214d5fed00
Elias Bachaalany <elias.bachaalany@gmail.com>
2024-04-03 20:40:43 -0700

    Update util_hotkeys.py
    added more hotkeys

--- plugins/util_hotkeys/util_hotkeys.py
+++ plugins/util_hotkeys/util_hotkeys.py
@@ -7,7 +7,32 @@ by Elias Bachaalany (c) AllThingsIDA
 """
 import idaapi
 
-UTIL_HOTKEYS = range(ord('A'), ord('Z') + 1)
+UTIL_HOTKEYS = [chr(i) for i in range(ord('A'), ord('Z') + 1)] + \
+               [chr(i) for i in range(ord('0'), ord('9') + 1)]
+
+UTILS_HOTKEYS_MAPPING = {
+    '/': 'slash',
+    '\\': 'backslash',
+    ';': 'semicolon',
+    ',': 'comma',
+    '.': 'dot',
+    '-': 'minus',
+    '=': 'equal',
+    "'": 'quote',
+    '[': 'lbracket',
+    ']': 'rbracket',
+    '`': 'backquote',
+    "Backspace": 'backspace',
+    '*': 'star',
+}
+
+UTIL_HOTKEYS.extend(UTILS_HOTKEYS_MAPPING.keys())
+UTIL_HOTKEYS.extend([f"F{i}" for i in range(1, 13)])
+UTIL_HOTKEYS.extend([
+    'Up', 'Down', 'Left', 'Right', 
+    'Enter', 'End', 'Space', 
+    'Insert', 'Home', 'Delete', 
+])
 
 class UtilHotkeys_plugmod_t(idaapi.plugmod_t):
     def __init__(self):
@@ -15,11 +40,11 @@ class UtilHotkeys_plugmod_t(idaapi.plugmod_t):
         print('--- UtilHotkeys --- started ---')
         msgs = []
         for key in UTIL_HOTKEYS:
-            func_name = f"utilhotkey_{chr(key).lower()}"
-            hotkey = f"Ctrl+Alt+Shift-{chr(key)}"
-            msgs.append(f"{hotkey} --> idaapi.{func_name}")
+            hotkey = f"Ctrl+Alt+Shift-{key}"
+            func_name = f"utilhotkey_{UTILS_HOTKEYS_MAPPING.get(key, key.lower())}"
             idaapi.add_hotkey(hotkey, lambda fn=func_name: eval(f"idaapi.{fn}()"))
-            setattr(idaapi, func_name, lambda key=key: print(f'placeholder for {chr(key)}'))
+            setattr(idaapi, func_name, lambda key=key: print(f'placeholder for {key}'))
+            msgs.append(f"{hotkey} --> idaapi.{func_name}")
         
         print('\n'.join(msgs))
         print('--- UtilHotkeys --- hotkeys registered ---')
